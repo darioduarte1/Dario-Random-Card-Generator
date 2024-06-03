e840a0841ad883dfaddf84085d5c3e0445718c5e
darioduarte1 <darioduarte1991@gmail.com>
2024-06-03 11:12:59 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let arrayNaipes = ["♦️", "♥️", "♠️", "♣️"];
+  let arrayNumeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let selecionNaipe =
+    arrayNaipes[Math.floor(Math.random() * arrayNaipes.length)];
+  let selecionNumeros =
+    arrayNumeros[Math.floor(Math.random() * arrayNumeros.length)];
+
+  let naipe1 = document.querySelector(".naipe1");
+  naipe1.innerHTML = selecionNaipe;
+
+  let naipe2 = document.querySelector(".naipe2");
+  naipe2.innerHTML = selecionNaipe;
+
+  if (selecionNaipe == "♦️" || selecionNaipe == "♥️") {
+    naipe1.classList.add("text-danger");
+    naipe2.classList.add("text-danger");
+  }
+
+  let numeros = document.querySelector("#numero");
+  numeros.innerHTML = selecionNumeros;
 };
